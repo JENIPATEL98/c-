7ee828951a2e4522c1c5738d2f9d6b0cf3ced3e2
JENIPATEL98 <pateljeni982006@gmail.com>
2025-05-24 09:50:21 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  // Student data
+  int ID = 15;
+  int Age = 23;
+  float Fee = 75.25;
+  char Grade = 'B';
+
+  // Print variables
+  printf("Student id: %d\n",ID);
+  printf("Student age: %d\n",Age);
+  printf("Student fee: %f\n",Fee);
+  printf("Student grade: %c", Grade);
+
+  return 0;
+}
